a67946f57bb4302b8482e416c22810f6c8e46a28
Force <naphatnaphat85@gmail.com>
2023-09-25 11:07:31 +0700

    add previous pointe

--- linkedlist.js
+++ linkedlist.js
@@ -2,7 +2,8 @@ class Linkedlist{
     constructor(value){
         this.head = {
             value: value,
-            next: null
+            next: null,
+            prev: null
         }
         this.tail = this.head
         this.length = 1;
@@ -10,8 +11,10 @@ class Linkedlist{
     append(value){
         const newNode = {
             value: value,
-            next: null
+            next: null,
+            prev: null
         }
+        newNode.prev = this.tail
         this.tail.next = newNode
         this.tail = newNode
         this.length++
@@ -19,8 +22,10 @@ class Linkedlist{
     prepend(value){
         const headNode = {
             value: value,
-            next: this.head
+            next: this.head,
+            prev: null
         }
+        this.head.prev = headNode
         this.head = headNode
         this.length++
     }
@@ -36,25 +41,49 @@ class Linkedlist{
     insert(index,value) {
         const insertNode = {
             value: value,
-            next: null
+            next: null,
+            prev: null
         }
+
+        if (index === 0) {
+            this.prepend(value)
+            return this.printList()
+        }
+        if (index >= this.length) {
+            this.append(value)
+            return this.printList()
+        }
+
         let currentNode = this.head;
         let i = 0;
         while(i < index-1){
             currentNode = currentNode.next
             i++
         }
+        insertNode.prev = currentNode
         const holdingPointer = currentNode.next
         currentNode.next = insertNode
         insertNode.next = holdingPointer
+        holdingPointer.prev = insertNode
         this.length++
+        return this.printList()
     }
     remove(index){
         let i = 0;
         if (index === 0) {
             this.head = this.head.next
+            this.head.prev = null
             this.length--
             return this.printList()
+        }else if (index >= this.length) {
+            let currentNode = this.head;
+            while(currentNode.next !== null){
+                currentNode = currentNode.next
+            }
+            currentNode.next = null
+            this.tail = currentNode
+            this.length--
+            return this.printList() 
         }
         let currentNode = this.head;
         while(i<index-1){
@@ -76,6 +105,7 @@ myLinkedList.append(5);
 myLinkedList.append(16);
 myLinkedList.prepend(1);
 myLinkedList.insert(2,99)
-myLinkedList.remove(2)
-myLinkedList.remove(0)
-console.log(myLinkedList.printList())
+console.log(myLinkedList)
+// myLinkedList.remove(2)
+// myLinkedList.remove(0)
+// console.log(myLinkedList.printList())
